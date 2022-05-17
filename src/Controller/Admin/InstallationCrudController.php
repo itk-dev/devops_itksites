b1b3782c72dd08ac8d54898aa5ac3fa62cdd5686
Ture Gjørup <ture@ignatz.dk>
2022-05-17 13:19:31 +0200

    SERV-500: Added filters to index pages

--- src/Controller/Admin/InstallationCrudController.php
+++ src/Controller/Admin/InstallationCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Installation;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -42,5 +43,13 @@ class InstallationCrudController extends AbstractCrudController
         yield AssociationField::new('detectionResult')->hideOnIndex();
         yield DateTimeField::new('createdAt');
         yield DateTimeField::new('detectionResult.lastContact')->hideOnIndex();
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('rootDir')
+            ->add('server')
+            ;
     }
 }
