d9fa0129f2e1fdb5af6dbef5fcc542d37df82af1
Mikkel Ricky <rimi@aarhus.dk>
2023-08-01 10:52:53 +0200

    ITKS-17: Consolidated migrations

--- migrations/Version20230801085216.php
+++ migrations/Version20230801085216.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230801074334 extends AbstractMigration
+final class Version20230801085216 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20230801074334 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE oidc ADD domain VARCHAR(255) NOT NULL, ADD expiration_time DATETIME NOT NULL, ADD one_password_url VARCHAR(255) NOT NULL, ADD usage_documentation_url VARCHAR(255) NOT NULL, DROP site, DROP expiration_date, DROP claims, DROP ad, DROP discovery_url, DROP graph');
         $this->addSql('ALTER TABLE service_certificate_service DROP system_uuid');
     }
 
@@ -27,5 +28,6 @@ final class Version20230801074334 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE service_certificate_service ADD system_uuid BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\'');
+        $this->addSql('ALTER TABLE oidc ADD site VARCHAR(255) NOT NULL, ADD expiration_date DATE NOT NULL, ADD claims LONGTEXT NOT NULL, ADD ad LONGTEXT NOT NULL, ADD discovery_url VARCHAR(255) NOT NULL, ADD graph VARCHAR(255) DEFAULT NULL, DROP domain, DROP expiration_time, DROP one_password_url, DROP usage_documentation_url');
     }
 }
