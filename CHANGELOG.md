00116d44c47a613db7f3efed0625175600dc3e99
Jesper Kristensen <cableman@linuxdev.dk>
2024-06-18 14:20:32 +0200

    1722: Added new types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Updated composer setup
+- Added new GPU hosts and Hetzner
+
 ## [1.6.0] - 2024-01-16
 
 - [#43](https://github.com/itk-dev/devops_itksites/pull/43)
--- src/Types/FrameworkTypes.php
+++ src/Types/FrameworkTypes.php
@@ -9,12 +9,14 @@ class FrameworkTypes
     public const DRUPAL = 'drupal';
     public const NODE = 'node';
     public const SYMFONY = 'symfony';
+    public const PYTHON = 'python';
     public const UNKNOWN = 'unknown';
 
     public const CHOICES = [
         'Drupal' => self::DRUPAL,
         'Node' => self::NODE,
         'Symfony' => self::SYMFONY,
+        'Python' => self::PYTHON,
         'Unknown' => self::UNKNOWN,
     ];
 }
