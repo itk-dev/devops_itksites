ac76826d22464e3107e092d406cef85859407432
turegjorup <ture@ignatz.dk>
2025-02-06 09:38:16 +0100

    3666: Add advisory count to Sites

--- src/Controller/Admin/SiteCrudController.php
+++ src/Controller/Admin/SiteCrudController.php
@@ -21,7 +21,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 
 class SiteCrudController extends AbstractCrudController
 {
