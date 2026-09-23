e77681f3b894d8115714a2f078cf444b21b955a6
turegjorup <ture@ignatz.dk>
2026-09-23 10:53:26 +0200

    feat: map drupal.org version constraints to Composer
    Adds DrupalVersion::constraintToComposer() for SA affected versions.

--- src/Utils/DrupalVersion.php
+++ src/Utils/DrupalVersion.php
@@ -20,4 +20,12 @@ class DrupalVersion
 
         return preg_replace('/^\d+\.x-(\d+)\.(\d+)(-.+)?$/', '$1.$2.0$3', $drupal);
     }
+
+    /**
+     * Maps every version in a drupal.org constraint, e.g. "<8.x-1.19 || >=2.0.0 <2.2.4".
+     */
+    public static function constraintToComposer(string $constraint): string
+    {
+        return (string) preg_replace('/\b\d+\.x-(\d+)\.(\d+)(-[0-9A-Za-z.]+)?/', '$1.$2.0$3', $constraint);
+    }
 }
--- tests/Utils/DrupalVersionTest.php
+++ tests/Utils/DrupalVersionTest.php
@@ -27,4 +27,21 @@ class DrupalVersionTest extends TestCase
         yield 'semver pre-release' => ['6.3.0-beta6', '6.3.0-beta6'];
         yield 'empty' => ['', null];
     }
+
+    #[DataProvider('constraintProvider')]
+    public function testConstraintToComposer(string $drupal, string $expected): void
+    {
+        self::assertSame($expected, DrupalVersion::constraintToComposer($drupal));
+    }
+
+    /**
+     * @return iterable<string, array{string, string}>
+     */
+    public static function constraintProvider(): iterable
+    {
+        yield 'semver' => ['<2.2.4', '<2.2.4'];
+        yield 'legacy' => ['<8.x-1.19', '<1.19.0'];
+        yield 'legacy pre-release' => ['>=8.x-1.0-beta1 <8.x-1.19', '>=1.0.0-beta1 <1.19.0'];
+        yield 'mixed' => ['<8.x-1.19 || >=2.0.0 <2.2.4', '<1.19.0 || >=2.0.0 <2.2.4'];
+    }
 }
